227051e7dc0e2920489405991d37f8ce372fb9c2
Alindas <60691143+AlindasUK@users.noreply.github.com>
2024-02-07 11:35:44 +0000

    Update bot.py

--- .circleci/bot.py
+++ .circleci/bot.py
@@ -1,9 +1,9 @@
-from javascript import require, On
-import discord
 from discord.ext import tasks, commands
+import discord
 import time
 from random import randint 
 import datetime
+
 now = datetime.datetime.now()
 displaytime = now.strftime("%Y-%m-%d %H:%M:%S")
 
@@ -13,7 +13,7 @@ msglog = {}
 dcbot = commands.Bot(command_prefix='>')
 dcbot.remove_command('help')
 
-colourlist = [0x1742cf,0xd9271a,0x2dd91a,0x961ad9,0xd91a86]
+colourlist = [0x1742cf, 0xd9271a, 0x2dd91a, 0x961ad9, 0xd91a86]
 
 global sendlist
 sendlist = []
@@ -34,74 +34,62 @@ async def senditall():
 
 @dcbot.event
 async def on_message(message):
-    if (str(message.channel.id) == "channelid")and(message.author.id != authorid):
+    if (str(message.channel.id) == "channelid") and (message.author.id != authorid):
         if "mfbot" in globals():
             mfbot.chat(f"[{message.author.name} > {message.content}]")
-    elif (str(message.channel.id) == "channelid")and(message.author.id != authorid):
+    elif (str(message.channel.id) == "channelid") and (message.author.id != authorid):
         if "mfbot" in globals():
             mfbot.chat(f"[{message.author.name} > {message.content}]")
-#on ready
+
 @dcbot.event
 async def on_ready():
     senditall.start()
 
-    channel = dcbot.get_channel()
+    # You need to replace the channelid with the actual channel ID where you want to send messages.
+    channel = dcbot.get_channel(channelid)
 
     global mineflayer
     global pathfinder
-    
+
     mineflayer = require('mineflayer')
     pathfinder = require('mineflayer-pathfinder')
-
-    
 
     RANGE_GOAL = 1
     BOT_USERNAME = 'mail'
 
     global mfbot
-    
 
     mfbot = mineflayer.createBot({
         'host': 'serverip',
-        #'port': 25565,
         'username': "username",
         'password': 'password',
         'version': 'version',
         'auth': 'microsoft'
-        })
+    })
     mfbot.loadPlugin(pathfinder.pathfinder)
-
-
 
     @On(mfbot, 'spawn')
     def handle(*args):
-        
         mcData = require('minecraft-data')(mfbot.version)
         movements = pathfinder.Movements(mfbot, mcData)
 
         pos = mfbot.entity.position
 
-        x = [ pos.x + 1 , pos.y , pos.z + 1 ]
+        x = [pos.x + 1, pos.y, pos.z + 1]
         mfbot.pathfinder.setMovements(movements)
         mfbot.pathfinder.setGoal(pathfinder.goals.GoalNear(x[0], x[1], x[2], RANGE_GOAL))
-        print(f"Bot Connected to 0b0t.org with version 1.12.2")
+        print(f"Bot Connected to server")
 
     @On(mfbot, 'chat')
     def handleMsg(this, sender, message, *args):
-        #checks wheather message appears in message log, if true, skips the message
         if str(int(time.time()/10)) + str(message) in msglog.keys():
             pass
         else:
-
-        
-            #adds the message to log to prevent duplicates
             msglog[str(int(time.time()/10)) + str(message)] = True
-            
             if (sender != "botname") and (sender != "Server"):
                 msg = f"{message} \n{displaytime}"
-
                 print("tries to append sendlist")
-                sendlist.append(discord.Embed(title=sender,description=msg, color=colourlist[randint(0,4)]))
+                sendlist.append(discord.Embed(title=sender, description=msg, color=colourlist[randint(0, 4)]))
 
-
-dcbot.run("")
+# Replace with your actual bot token
+dcbot.run("YOUR_BOT_TOKEN")
